b78076f0b19063b85f38e83aafca838b018102a5
Ricardo Carballo <riccar@protonmail.com>
2019-07-11 20:41:20 +1000

    Completed counter app

--- caloriecounter/src/Update.js
+++ caloriecounter/src/Update.js
@@ -3,30 +3,17 @@ import * as R from 'ramda';
 export const actionType = {
   SHOW_FORM: 'SHOW_FORM',
   MEAL_INPUT: 'MEAL_INPUT',
-  CALORIES_INPUT: 'CALORIES_INPUT'
+  CALORIES_INPUT: 'CALORIES_INPUT',
+  SAVE_MEAL: 'SAVE_MEAL',
+  EDIT_MEAL: 'EDIT_MEAL',
+  DELETE_MEAL: 'DELETE_MEAL'
 };
 
-// export function showFormAction(showForm) {
-//   return {
-//     type: actionType.SHOW_FORM,
-//     showForm
-//   };
-// }
-
-// export function mealInputAction(description) {
-//   return {
-//     type: actionType.MEAL_INPUT,
-//     description
-//   };
-// }
-
-// export function caloriesInputAction(calories) {
-//   return {
-//     type: actionType.CALORIES_INPUT,
-//     calories
-//   };
-// }
-
+/**
+ * Creates an action object with a type and a payload
+ * @param string type
+ * @param object payload
+ */
 export function action(type, payload) {
   return {
     type,
@@ -34,19 +21,88 @@ export function action(type, payload) {
   };
 }
 
+/**
+ * Add a meal to the meals array
+ * @param object model
+ */
+
+function add(model) {
+  const { nextId, description, calories } = model;
+  const meal = { id: nextId, description, calories };
+  const meals = [...model.meals, meal];
+  return {
+    ...model,
+    meals,
+    nextId: nextId + 1,
+    description: '',
+    calories: 0,
+    showForm: false
+  };
+}
+
+/**
+ * Updates meal within meals list matching updateId
+ * @param object model
+ */
+function edit(model) {
+  const { editId, description, calories } = model;
+  const meals = R.map(meal => {
+    if (meal.id === editId) {
+      return { ...meal, description, calories };
+    }
+    return meal;
+  }, model.meals);
+
+  return {
+    ...model,
+    meals,
+    description: '',
+    calories: 0,
+    showForm: false,
+    editId: null
+  };
+}
+
+/**
+ * Update model for selected meal to be edited
+ * @param object model
+ * @param number editId
+ */
+
+function setForEdit(model, editId) {
+  const meal = R.find(meal => meal.id === editId, model.meals);
+  const { description, calories } = meal;
+
+  return { ...model, description, calories, showForm: true, editId };
+}
+
 function update(action, model) {
   const { type, payload } = action;
   switch (type) {
     case actionType.SHOW_FORM:
       return { ...model, showForm: payload, description: '', calories: 0 };
+
     case actionType.MEAL_INPUT:
       return { ...model, description: payload };
+
     case actionType.CALORIES_INPUT:
       const calories = R.pipe(
         parseInt,
         R.defaultTo(0) //If parseInt returns NaN, then default it to 0
       )(payload);
       return { ...model, calories };
+
+    case actionType.SAVE_MEAL:
+      const { editId } = model;
+      const updatedModel = editId !== null ? edit(model) : add(model);
+      return updatedModel;
+
+    case actionType.EDIT_MEAL:
+      return setForEdit(model, payload);
+
+    case actionType.DELETE_MEAL:
+      const meals = R.filter(meal => meal.id !== payload, model.meals);
+      return { ...model, meals };
   }
   return model;
 }
--- caloriecounter/src/View.js
+++ caloriecounter/src/View.js
@@ -1,9 +1,26 @@
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
+import * as R from 'ramda';
+
 import { action, actionType } from './Update';
 
 //Destruct all the needed functions from hyperscript
-const { pre, div, h1, button, form, input, label } = hh(h);
+const {
+  pre,
+  div,
+  h1,
+  button,
+  form,
+  input,
+  label,
+  td,
+  th,
+  tr,
+  tbody,
+  thead,
+  table,
+  i
+} = hh(h);
 
 /**
  * Return a HTML form field
@@ -42,10 +59,23 @@ function buttonSet(dispatch) {
       {
         className: 'f3 pv2 ph3 bn bg-light-gray dim',
         type: 'button',
-        onclick: () => dispatch(action(actionType.SHOW_FORM, false))
+        onclick: () => dispatch(action(actionType.SHOW_FORM))
       },
       'Cancel'
     )
+  ]);
+}
+
+function mealButtonSet(dispatch, mealId) {
+  return div([
+    i({
+      className: 'ph1 fa fa-pencil-square-o pointer',
+      onclick: () => dispatch(action(actionType.EDIT_MEAL, mealId))
+    }),
+    i({
+      className: 'ph1 fa fa-trash-o pointer',
+      onclick: () => dispatch(action(actionType.DELETE_MEAL, mealId))
+    })
   ]);
 }
 
@@ -62,7 +92,11 @@ function formView(dispatch, model) {
     return form(
       {
         //width 100%, margin vertical 2
-        className: 'w-100 mv2'
+        className: 'w-100 mv2',
+        onsubmit: e => {
+          e.preventDefault();
+          if (description && calories) dispatch(action(actionType.SAVE_MEAL));
+        }
       },
       [
         fieldSet('Meal', description, e =>
@@ -87,6 +121,98 @@ function formView(dispatch, model) {
 }
 
 /**
+ * Returns HTML cel <tr> or <th> based on the given tag
+ * @param string tag: HTML cell tag
+ * @param string className: CSS classes
+ * @param string value: Cell containing value
+ */
+
+function cell(tag, className, value) {
+  return tag({ className }, value);
+}
+
+/**
+ * Returns HTML row based on calls to the cell value
+ * @param string className: CSS classes
+ * @param object meal: Meal object to be display per row
+ */
+
+function mealRow(dispatch, className, meal) {
+  return tr({ className }, [
+    cell(td, 'pa2', meal.description),
+    cell(td, 'pa2 tr', meal.calories),
+    cell(td, 'pa2 tr', mealButtonSet(dispatch, meal.id))
+  ]);
+}
+
+/**
+ * Returns table body, produced by mapping through the meals and
+ * calling a mealRow per meal and finally appending the last row with meal total
+ * @param string className
+ * @param array meals
+ */
+function mealsBody(dispatch, className, meals) {
+  const rows = R.map(R.partial(mealRow, [dispatch, 'stripe-dark']), meals);
+
+  const rowsWithTotal = [...rows, totalRow(meals)];
+
+  return tbody({ className }, rowsWithTotal);
+}
+
+/**
+ * Const headerRow consist on calling 3 cells with header labels
+ */
+const headerRow = tr([
+  cell(th, 'pa2 tl', 'Meal'),
+  cell(th, 'pa2 tr', 'Calories'),
+  cell(th, '', '')
+]);
+
+/**
+ * Const mealsHeader contains the headerRows within HTML <thead> tag
+ */
+const mealsHeader = thead(headerRow);
+
+/**
+ * returns the last row with the sum of all the meal calories
+ * @param object meals: Object with meals
+ */
+
+function totalRow(meals) {
+  const total = R.pipe(
+    //Partially applying map that returns an array of calories
+    R.map(meal => meal.calories),
+    //pipe sends the array of calories to the reduce funtion that takes two params
+    //the first is the transformation function that add the calories
+    //the second is the acc initial value of 0
+    R.reduce((acc, calories) => acc + calories, 0)
+  )(meals); //meals is the last parameter pipe receives which is
+  //the data it will act upon.
+  return tr({ className: 'bt b' }, [
+    cell(td, 'pa2 tr', 'Total:'),
+    cell(td, 'pa2 tr', total),
+    cell(td, '', '')
+  ]);
+}
+
+/**
+ * Returns the HTML table produced by calling the mealsHeader and mealsBody passing
+ * the list of meals
+ * @param string className: CSS classes
+ * @param array meals: list of meals
+ */
+
+function mealTable(dispatch, meals) {
+  if (meals.length === 0) {
+    return div({ className: 'mv2 i black-50' }, 'No meals to display...');
+  }
+  return table({ className: 'mv2 center w-100 collapse' }, [
+    mealsHeader,
+    mealsBody(dispatch, '', meals)
+  ]);
+}
+
+/**
  * Returns the application view
  * @param function dispatch
  * @param object model
@@ -97,6 +223,7 @@ function view(dispatch, model) {
     h1({ className: 'f2 pv2 bb' }, 'Calorie Counter'),
     //print app model as a json data with 2 space indentation.
     formView(dispatch, model),
+    mealTable(dispatch, model.meals),
     pre(JSON.stringify(model, null, 2))
   ]);
 }
